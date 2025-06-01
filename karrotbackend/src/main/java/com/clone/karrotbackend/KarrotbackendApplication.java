d27009abb14b3f62bc9658c92593f9704f71308d
kimnuri <healop@naver.com>
2025-06-01 13:05:18 +0900

    Initial commit - npm -> yarn berry

---
+++ karrotbackend/src/main/java/com/clone/karrotbackend/KarrotbackendApplication.java
@@ -0,0 +1,13 @@
+package com.clone.karrotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrotbackendApplication.class, args);
+	}
+
+}
---
+++ karrotbackend/src/test/java/com/clone/karrotbackend/KarrotbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.karrotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
